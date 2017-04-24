013f4dc718b26b51b7524947fb6f05a677a57774
stacyz <stacyz@uw.edu>
2017-04-23 22:19:39 -0700

    updated 04/23 10pm

--- js/script.js
+++ js/script.js
@@ -1,20 +1,27 @@
 $(function() {
 	d3.csv('data/child-death-rate-by-causes.csv', function(error, allData) {
 
-		var width = 960,
-			height = 500,
-			yr = '2015';
-
-
 
 		var years = allData.map(function(d) { return(d['Year']); });
 		var uniqueYear = years.filter(function(elem, pos) { return years.indexOf(elem) == pos; });
-		//console.log(uniqueYear);
+		console.log(uniqueYear);
 		uniqueYear.map(function(d) { 
 			var option = $('section').append('<option></option>');
 			$('option').attr('value', d);
 		});
 		
+		var width = 960,
+			height = 500,
+			yr = '2015';
+
+		var x = d3.scaleLinear()
+			.range([0, width]);
+
+		var y = d3.scaleLinear()
+			.range([0, height]);
+
+		var color = d3.scaleOrdinal(d3.schemeCategory20c);
+
 		var filterData = function() {
 			var currentData = allData.filter(function(d) {
 				return d.Year == yr;
@@ -22,7 +29,7 @@ $(function() {
 			return currentData;
 		};
 
-		var nestData = function(data) {
+		/*var nestData = function(data) {
 			d3.nest()
 			.key(function(d) {
 				return d.Cause;
@@ -38,20 +45,80 @@ $(function() {
 				return d.values;
 			});
 		}
-		//console.log(root);
+		//console.log(root);*/
 
 		var partition = d3.partition()
 			.size([width, height])
+			.padding(0)
+			.round(true);
+
+		var svg = d3.select('#vis').append('svg')
+			.attr('width', width)
+			.attr('height', height);
+
+		var rects = svg.selectAll('rect');
 
 		var draw = function(data) {
-			var nestedData = nestData(data);
-			var root = getRoot(data);
+			var nestedData = d3.nest().key(function(d) {
+				return d.Cause;
+			})
+			.entries(data);
+			//console.log(nestedData);
+
+			var root = d3.hierarchy({
+				values: nestedData
+			}, function(d) {
+				d.values;
+			})
+			.sum(function(d) { return d.value; });
 			
-			root.sum(data);
 
 			partition(root);
+			console.log(root);
+
+			/*rects = rects.data(root.descendants())
+				.enter().append('rect')
+				.attr('x', function(d) { return d.x0; })
+				.attr('y', function(d) { return d.y0; })
+				.attr('width', function(d) {return d.x1 - d.x0; })
+				.attr('height', function(d) { return d.y1 - d.y0; })
+				.attr('fill', function(d) { return color((d.children ? d:d.parent).data.key); })
+				.on('click', clicked);*/
+
+
+			/*d3.json(nestedData, function(error, root) {
+				//if (error) throw error;
+				console.log(root);
+				root = d3.hierarchy(d3.entries(root)[0], function(d) {
+					return d3.entries(d.value)
+				})
+					.sum(function(d) { return d.value; });
+
+				partition(root);
+
+				rects = rects.data(root.descendants())
+					.enter().append('rect')
+					.attr('x', function(d) { return d.x0; })
+					.attr('y', function(d) { return d.y0; })
+					.attr('width', function(d) {return d.x1 - d.x0; })
+					.attr('height', function(d) { return d.y1 - d.y0; })
+					.attr('fill', function(d) { return color((d.children ? d:d.parent).data.key); })
+					.on('click', clicked);
+			});*/
+
+
 
 		}
+
+		$('input').on('change', function() {
+			yr = $(this).val();
+
+			var currentData = filterData();
+			draw(currentData);
+		});
+
+		var currentData = filterData();
+		draw(currentData);
 
 
 
